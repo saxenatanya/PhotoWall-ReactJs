81e7d8fdf6f8d3de7470bc222ffdef6c655b59fe
saxenatanya <82470912+saxenatanya@users.noreply.github.com>
2021-06-14 11:41:34 -0400

    Create Title.js

---
+++ src/components/Title.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+function Title(props) {
+    return <h1>{props.title}</h1>
+}
+
+
+// class Title extends Component {
+
+//     render(){
+       
+//     }
+// }
+
+export default Title
